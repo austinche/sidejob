b02c80743bd8e28dd53e9d582cee19b14337c881
Austin Che <austin@ginkgobioworks.com>
2014-04-02 16:55:47 -0400

    change graph syntax to eliminate class arguments

--- lib/sidejob/parser.rb
+++ lib/sidejob/parser.rb
@@ -5,34 +5,33 @@ require 'parslet'
 module SideJob
   class Parser < Parslet::Parser
     class Transform < Parslet::Transform
+      # unescape strings
+      rule(str: simple(:str)) do
+        { str: str.to_s.gsub(/\\(.)/, "\\1") }
+      end
+
       # handle job definitions
-      rule(name: simple(:name), value: simple(:value)) { [name.to_s, value.to_s] }
-      rule(line: { job_definition: { job: simple(:job), queue: simple(:queue), class: simple(:job_class), args: subtree(:args) }}) do
-        merged = args.inject({}) do |h, arg|
-          key = arg[0]
-          val = arg[1]
-          raise "#{job}: argument #{key} duplicated" if h[key]
-          h[key.to_s] = val
-          h
-        end
-        { type: :job, job: job.to_s, queue: queue.to_s, class: job_class.to_s, args: merged }
+      rule(line: { job_definition: { job: simple(:job), queue: simple(:queue), class: simple(:job_class) }}) do
+        { type: :job, job: job.to_s, queue: queue.to_s, class: job_class.to_s }
       end
 
       # handle connections
-      rule(job: simple(:job)) { job }
-      rule(port: simple(:port)) { port }
-      rule(line: { connection: { src_job: simple(:src_job), src_port: simple(:src_port), tgt_job: simple(:tgt_job), tgt_port: simple(:tgt_port), next: subtree(:next_link), split: subtree(:split) }}) do
-        connections = [ {src: {job: src_job.to_s, port: src_port.to_s}, tgt: {job: tgt_job.to_s, port: tgt_port.to_s}} ]
+      rule(job: simple(:job), port: simple(:port)) { {job: job.to_s, port: port.to_s} }
+      rule(job: simple(:job)) { job.to_s }
+      rule(port: simple(:port)) { port.to_s }
+      rule(line: { connection: { source: subtree(:source), tgt_job: simple(:tgt_job), tgt_port: simple(:tgt_port), next: subtree(:next_link), split: subtree(:split) }}) do
+        last_source = source
+        connections = [ {src: last_source, tgt: {job: tgt_job.to_s, port: tgt_port.to_s}} ]
+
         prev_job = tgt_job.to_s
-        last_outport = [src_job.to_s, src_port.to_s]
         next_link.each do |port|
           connections << {src: {job: prev_job, port: port[:src_port].to_s}, tgt: {job: port[:tgt_job].to_s, port: port[:tgt_port].to_s}}
-          last_outport = [prev_job, port[:src_port].to_s]
+          last_source = {job: prev_job, port: port[:src_port].to_s}
           prev_job = port[:tgt_job].to_s
         end
 
         split.each do |other|
-          connections << {src: {job: last_outport[0], port: last_outport[1]}, tgt: {job: other[:job].to_s, port: other[:port].to_s}}
+          connections << {src: last_source, tgt: {job: other[:job].to_s, port: other[:port].to_s}}
         end
 
         {type: :connections, connections: connections}
@@ -60,10 +59,8 @@ module SideJob
             when :job
               raise "#{line[:job]}: duplicate definition" if jobs[line[:job]]
               jobs[line[:job]] = { 'queue' => line[:queue], 'class' => line[:class] }
-              if line[:args] && line[:args].size > 0
-                jobs[line[:job]]['args'] = line[:args]
-              end
             when :connections
+              # handle connections after all jobs
               connections.concat line[:connections]
             else
               raise "Unable to transform line: #{line}"
@@ -82,11 +79,18 @@ module SideJob
             outports[tgt[:port]] ||= []
             outports[tgt[:port]] << {'job' => src[:job], 'port' => src[:port]}
           else
-            raise "Undefined job #{src[:job]}" unless jobs[src[:job]]
             raise "Undefined job #{tgt[:job]}" unless jobs[tgt[:job]]
-            jobs[src[:job]]['connections'] ||= {}
-            jobs[src[:job]]['connections'][src[:port]] ||= []
-            jobs[src[:job]]['connections'][src[:port]] << {'job' => tgt[:job], 'port' => tgt[:port]}
+
+            if src[:str]
+              jobs[tgt[:job]]['init'] ||= {}
+              jobs[tgt[:job]]['init'][tgt[:port]] ||= []
+              jobs[tgt[:job]]['init'][tgt[:port]] << src[:str]
+            else
+              raise "Undefined job #{src[:job]}" unless jobs[src[:job]]
+              jobs[src[:job]]['connections'] ||= {}
+              jobs[src[:job]]['connections'][src[:port]] ||= []
+              jobs[src[:job]]['connections'][src[:port]] << {'job' => tgt[:job], 'port' => tgt[:port]}
+            end
           end
         end
 
@@ -106,24 +110,23 @@ module SideJob
     rule(:queue_name) { safe_name }
     rule(:class_name) { (safe_name | match[':.']).repeat(1) }
     rule(:job_name) { str('[') >> safe_name.as(:job) >> str(']') }
-    rule(:job_name_or_self) { str('@') | job_name }
+    rule(:job_name_or_self) { str('@').as(:job) | job_name }
     rule(:port_name) { safe_name.as(:port) }
-    rule(:argument_name) { safe_name }
     rule(:inport) { port_name >> str(':') }
     rule(:outport) { str(':') >> port_name }
 
-    rule(:connection) { outport.as(:src_port) >> whitespace >> str('->') >> whitespace >> inport.as(:tgt_port) >> job_name_or_self.as(:tgt_job) }
-    rule(:connection_chain) { connection.repeat }
-    # split connection only allowed at end of chain
-    rule(:connection_split) { whitespace >> str('+') >> whitespace >> inport >> job_name_or_self }
-    rule(:connections) { job_name_or_self.as(:src_job) >> connection >> connection.repeat.as(:next) >> connection_split.repeat.as(:split) }
+    rule(:quote) { str("'") }
+    rule(:string) { quote >> ((str('\\') | quote.absent?) >> any).repeat.as(:str) >> quote }
 
-    rule(:matching_quote) { dynamic {|s, c| str(c.captures[:quote])} }
-    rule(:quoted_string) { match['\'"'].capture(:quote) >> ((matching_quote.absent? >> any).repeat).as(:value) >> matching_quote }
-    rule(:unquoted_string) { (str("'") | str('"')).absent? >> (space_or_newline.absent? >> any).repeat(1) }
-    rule(:string) { unquoted_string.as(:value) | quoted_string }
-    rule(:job_argument) { whitespace >> argument_name.as(:name) >> str(':') >> string }
-    rule(:job_definition) { job_name >> whitespace >> str('=') >> whitespace >> queue_name.as(:queue) >> whitespace >> class_name.as(:class) >> job_argument.repeat.as(:args) }
+    rule(:connection_source) { job_name_or_self >> outport }
+    rule(:connection_target) { whitespace >> str('->') >> whitespace >> inport.as(:tgt_port) >> job_name_or_self.as(:tgt_job) }
+    rule(:connection_next) { outport.as(:src_port) >> whitespace >> connection_target }
+    rule(:connection_split) { whitespace >> str('+') >> whitespace >> inport >> job_name_or_self }
+    # string/init data only allowed at beginning
+    # split connection only allowed at end of chain
+    rule(:connections) { (string | connection_source).as(:source) >> connection_target >> connection_next.repeat.as(:next) >> connection_split.repeat.as(:split) }
+
+    rule(:job_definition) { job_name >> whitespace >> str('=') >> whitespace >> queue_name.as(:queue) >> whitespace >> class_name.as(:class) }
 
     rule(:comment) { str('#') >> (newline.absent? >> any).repeat }
 
@@ -140,6 +143,52 @@ module SideJob
       Transform.new.apply(parsed)
     rescue Parslet::ParseFailed => error
       raise error.cause.ascii_tree
+    end
+
+    # Converts output of parse back into the flow graph language
+    # @param graph [Hash]
+    # @return [String]
+    def self.unparse(graph)
+      lines = []
+      graph['jobs'].each_pair do |name, info|
+        lines << "[#{name}] = #{info['queue']} #{info['class']}"
+
+        if info['connections']
+          info['connections'].each_pair do |outport, targets|
+            all_targets = []
+            targets.each do |target|
+              all_targets << "#{target['port']}:[#{target['job']}]"
+            end
+            lines << "[#{name}]:#{outport} -> #{all_targets.join(' + ')}"
+          end
+        end
+
+        if info['init']
+          info['init'].each_pair do |port, data|
+            data.each do |x|
+              lines << "'#{x.gsub("'") {|x| "\\'"}}' -> #{port}:[#{name}]"
+            end
+          end
+        end
+      end
+
+      if graph['inports']
+        graph['inports'].each_pair do |port, targets|
+          targets.each do |target|
+            lines << "@:#{port} -> #{target['port']}:[#{target['job']}]"
+          end
+        end
+      end
+
+      if graph['outports']
+        graph['outports'].each_pair do |port, sources|
+          sources.each do |source|
+            lines << "[#{source['job']}]:#{source['port']} -> #{port}:@"
+          end
+        end
+      end
+
+      lines.join("\n")
     end
   end
 end
--- spec/sidejob/parser_spec.rb
+++ spec/sidejob/parser_spec.rb
@@ -10,24 +10,22 @@ describe SideJob::Parser do
       expect(parser.comment).to parse('# hello world')
     end
 
-    it 'parses arguments' do
-      expect(parser.job_argument).to parse("arg:string")
-      expect(parser.job_argument).to parse("arg:'string'")
-      expect(parser.job_argument).to parse("arg:\"string\"")
-    end
-
     it 'parses job definition' do
       expect(parser.job_definition).not_to parse("[job] = q")
-      expect(parser.job_definition.parse("[job] = q class1")).to eq({job: 'job', queue: 'q', class: 'class1', args: []})
-      expect(parser.job_definition.parse("[JOB2] = Q2 Class2 arg:value")).to eq({job: 'JOB2', queue: 'Q2', class: 'Class2', args: [{name: 'arg', value: 'value'}]})
-      expect(parser.job_definition.parse("[job] = q class arg:'hello world' arg2:\"'!@#\"")).to eq({job: 'job', queue: 'q', class: 'class', args: [{name: 'arg', value: 'hello world'}, {name: 'arg2', value: "'!@#"}]})
+      expect(parser.job_definition.parse("[job] = q class1")).to eq({job: 'job', queue: 'q', class: 'class1'})
+      expect(parser.job_definition.parse("[JOB2] = Q2 Class2")).to eq({job: 'JOB2', queue: 'Q2', class: 'Class2'})
     end
 
     it 'parses connections' do
-      expect(parser.connections.parse("[job]:out -> in:[job2]")).to eq({src_job: {job: 'job'}, src_port: {port: 'out'}, tgt_job: {job: 'job2'}, tgt_port: {port: 'in'}, next: [], split: []})
-      expect(parser.connections.parse("[job]:out -> in:[job2]:out -> in2:[job3]")).to eq({src_job: {job: 'job'}, src_port: {port: 'out'}, tgt_job: {job: 'job2'}, tgt_port: {port: 'in'}, next: [{src_port: {port: 'out'}, tgt_port: {port: 'in2'}, tgt_job: {job: 'job3'}}], split: []})
-      expect(parser.connections.parse("[job]:out -> in:[job2] + in:[job3]")).to eq({src_job: {job: 'job'}, src_port: {port: 'out'}, tgt_job: {job: 'job2'}, tgt_port: {port: 'in'}, next: [], split: [{job: 'job3', port: 'in'}]})
-      expect(parser.connections.parse("[job]:out -> in:[job2]:out -> x:[job1] + y:[job2] + z:[job3]")).to eq({src_job: {job: 'job'}, src_port: {port: 'out'}, tgt_job: {job: 'job2'}, tgt_port: {port: 'in'}, next: [{src_port: {port: 'out'}, tgt_port: {port: 'x'}, tgt_job: {job: 'job1'}}], split: [{port: 'y', job: 'job2'}, {port: 'z', job: 'job3'}]})
+      expect(parser.connections.parse("[job]:out -> in:[job2]")).to eq({source: {job: 'job', port: 'out'}, tgt_job: {job: 'job2'}, tgt_port: {port: 'in'}, next: [], split: []})
+      expect(parser.connections.parse("[job]:out -> in:[job2]:out -> in2:[job3]")).to eq({source: {job: 'job', port: 'out'}, tgt_job: {job: 'job2'}, tgt_port: {port: 'in'}, next: [{src_port: {port: 'out'}, tgt_port: {port: 'in2'}, tgt_job: {job: 'job3'}}], split: []})
+      expect(parser.connections.parse("[job]:out -> in:[job2] + in:[job3]")).to eq({source: {job: 'job', port: 'out'}, tgt_job: {job: 'job2'}, tgt_port: {port: 'in'}, next: [], split: [{job: 'job3', port: 'in'}]})
+      expect(parser.connections.parse("[job]:out -> in:[job2]:out -> x:[job1] + y:[job2] + z:[job3]")).to eq({source: {job: 'job', port: 'out'}, tgt_job: {job: 'job2'}, tgt_port: {port: 'in'}, next: [{src_port: {port: 'out'}, tgt_port: {port: 'x'}, tgt_job: {job: 'job1'}}], split: [{port: 'y', job: 'job2'}, {port: 'z', job: 'job3'}]})
+    end
+
+    it 'parses initial data' do
+      expect(parser.connections.parse("'foo' -> in:[job]")).to eq({source: {str: 'foo'}, tgt_port: {port: 'in'}, tgt_job: {job: 'job'}, next: [], split: []})
+      expect(parser.connections).to parse("'abc' -> in:[job1]:out -> in2:[job2] + in3:[job3]")
     end
 
     it 'parses graph inports and outports' do
@@ -37,13 +35,31 @@ describe SideJob::Parser do
       expect(parser.connections).to parse('[job]:out -> output:@:input -> in:[job]')
     end
 
+    it 'parses strings' do
+      expect(parser.string).not_to parse("abc")
+      expect(parser.string).not_to parse('"abc"')
+      expect(parser.string.parse("'foo bar'")).to eq({str: 'foo bar'})
+      expect(parser.string.parse("'foo \"bar\"'")).to eq({str: 'foo "bar"'})
+      expect(parser.string.parse("'foo \"bar\"'")).to eq({str: 'foo "bar"'})
+      expect(parser.string.parse("'foo\nbar'")).to eq({str: "foo\nbar"})
+      expect(parser.string.parse("'foo\\'bar'")).to eq({str: "foo\\'bar"})
+    end
+
     it 'parses multiple lines' do
       expect(parser).to parse %Q{
         # parse this
-        [job3] = testq Foo::Bar foo:bar foo2:"hello world" # comment
+        [job3] = testq Foo::Bar # comment
+        'bar' -> foo:[job3]
         [job]:out -> in:[job2] + in:[job3]
         [job]:out -> in2:[job2]:out2 -> in3:[job3]
       }
+    end
+  end
+
+  context 'transformation rules' do
+    let(:transform) { SideJob::Parser::Transform.new }
+    it 'unescapes strings' do
+      expect(transform.apply(str: "foo\\'bar")).to eq({str: "foo'bar"})
     end
   end
 
@@ -55,31 +71,25 @@ describe SideJob::Parser do
 
     it 'can successfully parse jobs' do
       expect(SideJob::Parser.parse("[Job] = q c")).to eq({'jobs' => {'Job' => {'queue' => 'q', 'class' => 'c'}}})
-      expect(SideJob::Parser.parse("[Job] = q c arg:val")).to eq({'jobs' => {'Job' => {'queue' => 'q', 'class' => 'c', 'args' => { 'arg' => 'val' }}}})
-      expect(SideJob::Parser.parse("[Job] = q c arg1:val1
-        [Job1] = r d arg2:val2"
-        )).to eq({'jobs' => {'Job' => {'queue' => 'q', 'class' => 'c', 'args' => { 'arg1' => 'val1' }}, 'Job1' => {'queue' => 'r', 'class' => 'd', 'args' => { 'arg2' => 'val2' }}}})
     end
 
     it 'can successfully parse jobs and connections' do
       expect(SideJob::Parser.parse(%Q{
         # parse this
-        [Job1] = testq Foo::Bar arg:"hello world" # comment
-        [Job2] = testq Foo::Baz arg2:val
+        [Job1] = testq Foo::Bar
+        'hello world' -> port1:[Job1] # comment
+        [Job2] = testq Foo::Baz
+        'val' -> port2:[Job2]
         [Job1]:out -> in:[Job2]:out -> in2:[Job1]:out2 -> in3:[Job1] + in4:[Job1]
         [Job1]:out2 -> in2:[Job2] + x:[Job1]
       })).to eq({'jobs' => {
-        'Job1' => {'queue' => 'testq', 'class' => 'Foo::Bar', 'args' => { 'arg' => 'hello world' }, 'connections' => {'out' => [{'job' => 'Job2', 'port' => 'in'}], 'out2' => [{'job' => 'Job1', 'port' => 'in3'}, {'job' => 'Job1', 'port' => 'in4'}, {'job' => 'Job2', 'port' => 'in2'}, {'job' => 'Job1', 'port' => 'x'}]}},
-        'Job2' => {'queue' => 'testq', 'class' => 'Foo::Baz', 'args' => { 'arg2' => 'val' }, 'connections' => {'out' => [{'job' => 'Job1', 'port' => 'in2'}]}},
+        'Job1' => {'queue' => 'testq', 'class' => 'Foo::Bar', 'init' => { 'port1' => ['hello world'] }, 'connections' => {'out' => [{'job' => 'Job2', 'port' => 'in'}], 'out2' => [{'job' => 'Job1', 'port' => 'in3'}, {'job' => 'Job1', 'port' => 'in4'}, {'job' => 'Job2', 'port' => 'in2'}, {'job' => 'Job1', 'port' => 'x'}]}},
+        'Job2' => {'queue' => 'testq', 'class' => 'Foo::Baz', 'init' => { 'port2' => ['val'] }, 'connections' => {'out' => [{'job' => 'Job1', 'port' => 'in2'}]}},
       }})
     end
 
     it 'can specify graph inports and outports' do
       expect(SideJob::Parser.parse("[Job] = q c\n@:myIn -> in:[Job]:out -> myOut:@")).to eq({'jobs' => {'Job' => {'queue' => 'q', 'class' => 'c'}}, 'inports' => {'myIn' => [{'job' => 'Job', 'port' => 'in'}]}, 'outports' => {'myOut' => [{'job' => 'Job', 'port' => 'out'}]}})
-    end
-
-    it 'raises error if argument name is duplicated' do
-      expect { SideJob::Parser.parse("[Job] = q c x:1 x:2") }.to raise_error
     end
 
     it 'raises error if job is defined multiple times' do
@@ -88,6 +98,26 @@ describe SideJob::Parser do
 
     it 'raises error if connection refers to non-existent job' do
       expect { SideJob::Parser.parse("[Job]:out -> in:[Job]") }.to raise_error
+    end
+  end
+
+  describe '.unparse' do
+    it 'can successfully unparse empty graphs' do
+      expect(SideJob::Parser.unparse({'jobs' => {}})).to eq('')
+    end
+
+    it 'can successfully unparse jobs' do
+      expect(SideJob::Parser.unparse({'jobs' => {'Job' => {'queue' => 'q', 'class' => 'c', 'init' => { 'port1' => ["va'l"] }}, 'Job1' => {'queue' => 'r', 'class' => 'd', 'init' => { 'port2' => ['val2'] }}}})).to eq("[Job] = q c\n'va\\'l' -> port1:[Job]\n[Job1] = r d\n'val2' -> port2:[Job1]")
+    end
+
+    it 'can successfully unparse connections' do
+      expect(SideJob::Parser.unparse({'jobs' => {
+                 'Job1' => {'queue' => 'testq', 'class' => 'Foo::Bar', 'connections' => {'out' => [{'job' => 'Job1', 'port' => 'in'}], 'out2' => [{'job' => 'Job1', 'port' => 'in2'}, {'job' => 'Job1', 'port' => 'in3'}]}},
+      }})).to eq("[Job1] = testq Foo::Bar\n[Job1]:out -> in:[Job1]\n[Job1]:out2 -> in2:[Job1] + in3:[Job1]")
+    end
+
+    it 'can successfully unparse inports and outports' do
+      expect(SideJob::Parser.unparse({'jobs' => {'Job' => {'queue' => 'q', 'class' => 'c'}}, 'inports' => {'myIn' => [{'job' => 'Job', 'port' => 'in'}]}, 'outports' => {'myOut' => [{'job' => 'Job', 'port' => 'out'}]}})).to eq("[Job] = q c\n@:myIn -> in:[Job]\n[Job]:out -> myOut:@")
     end
   end
 end
